fd63965cd2e0d0700883f7e9a11155cb63ff25f3
zhanghongming <962397295@qq.com>
2015-08-13 17:23:58 +0800

    by zhanghongming

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
-echo"cdcdcdcdc";
-?>
+$arr=array(1,4,3,2,8,6,9,4);
+function quick_sort($arr) {
+$n=count($arr);
+if($n<=1)
+return $arr;
+$key=$arr[0];
+$left_arr=array();
+$right_arr=array();
+for($i=1;$i<$n;$i++) {
+if($arr[$i]<=$key)
+$left_arr[]=$arr[$i];
+else
+$right_arr[]=$arr[$i];
+}
+$left_arr=quick_sort($left_arr);
+$right_arr=quick_sort($right_arr);
+return array_merge($left_arr,array($key),$right_arr);
+}
