3a40ad64643738cb204b2ed7d9dc4e9403ecb385
zhanghongming <962397295@qq.com>
2015-08-22 12:09:14 +0800

    by zhanghongming

--- zxy.php
+++ zxy.php
@@ -1,6 +1,6 @@
 <?php
-$zxy =rand(1,100000);
-$mony = rand(0.5,5);
+
+
 ?>
 <form method="get" action="">
     请输入代号:<input type="text" name="val">
@@ -11,19 +11,52 @@ $mony = rand(0.5,5);
 <?php
     if($_GET['val']!=""){
         if($_GET['val']==1){
+            $zxy =rand(1,100000);
+            $mony1 = rand(0.5,5);
             ?>
-            <script language="JavaScript">
-                alert('张馨元大坏蛋哈哈哈哈！！！！');
-            </script>
 <?php
             echo"<font color=\"red\" size=\"28\"> 张馨元的大小:$zxy</font>";
             echo"<br>";
-            echo"<font color=\"red\" size=\"28\">钱数为: $mony</font>";
+            echo"<font color=\"red\" size=\"28\">钱数为: $mony1</font>";
         }else{
-            echo"<font color=\"red\" size=\"28\">张洪明的大小:$zxy</font>";
+            $zhm =rand(1,100000);
+            $mony = rand(0.5,5);
+            echo"<font color=\"red\" size=\"28\">张洪明的大小:$zhm</font>";
             echo"<br>";
            echo"<font color=\"red\" size=\"28\">  钱数为:$mony</font>";
         }
+
+
+        if($zxy > $zhm){
+            ?>
+            <script language="JavaScript">
+                alert('张辛苑赢了');
+            </script>
+<?php
+            echo"赢了$mony元";
+        }
+
+
+        if($zxy == $zhm){
+            ?>
+            <script language="JavaScript">
+                alert('平局');
+            </script>
+            <?php
+            echo"赢了0元";
+        }
+
+
+        if($zxy < $zhm){
+            ?>
+            <script language="JavaScript">
+                alert('张洪明赢了');
+            </script>
+            <?php
+            echo"赢了$mony1元";
+        }
+
+
     }
 ?>
 
